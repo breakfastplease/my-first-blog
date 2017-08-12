9dd2a18b8ef6a28e12775607a9ba0f7c6289acc7
kv <yourmail@example.com>
2017-08-11 18:37:44 -0700

    My Django App, second commit

--- mysite2/blog/models.py
+++ mysite2/blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+#from django.utils.encoding import python_2_unicode_compatible
 
-@python_2_unicode_compatible
+#@python_2_unicode_compatible
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
